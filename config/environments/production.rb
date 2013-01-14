2b4b2c34a889294b2f4f368c4c23226a55f0df79
Masaru Sato <marphy.986@gmail.com>
2013-01-14 15:19:30 +0900

    set config/environments/production.rb rails default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ Lgdpm::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -66,7 +66,6 @@ Lgdpm::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Dalli memcache client library settings
-  config.cache_store = :dalli_store
   # config.cache_store = :dalli_store, 'cache-1.example.com', 'cache-2.example.com',
   #   { :namespace => LGDPM, :expires_in => 1.day, :compress => true }
 end
