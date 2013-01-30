56bf1714442212818c5dbc3c96a45edd1eddb2b9
c-yasuda <yasuda.chihiro@adniss.jp>
2013-01-30 15:24:19 +0900

    dalli setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+require 'action_dispatch/middleware/session/dalli_store'
 Lgdpm::Application.config.session_store :cookie_store, key: '_LGDPM_session'
 
 # Use the database for sessions instead of the cookie-based default,
