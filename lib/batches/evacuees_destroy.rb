17cf7b23cf028c870265312779ab4c4deddd73a7
c-yasuda <yasuda.chihiro@adniss.jp>
2013-02-05 10:43:11 +0900

    削除バッチ追加

---
+++ lib/batches/evacuees_destroy.rb
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# 避難者情報削除バッチ
+# ==== バッチの実行コマンド
+# rails runner Batches::EvacueesDestroy.execute
+# ==== options
+# 実行環境の指定 :: -e production
+
+class Batches::EvacueesDestroy
+  def self.execute
+    Rails.logger.info(" #{Time.now.to_s} ===== #{self.name} START ===== ")
+    
+    Evacuee.destroy_all(["updated_at < ?", 1.year.ago])
+    LocalPerson.destroy_all(["updated_at < ?", 1.year.ago])
+    
+    Rails.logger.info(" #{Time.now.to_s} ===== #{self.name} END  ===== ")
+  end
+end
