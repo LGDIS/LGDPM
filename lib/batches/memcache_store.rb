7f88695bb6f7489a162dbe802562cdf8d01b28da
c-yasuda <yasuda.chihiro@adniss.jp>
2013-02-14 20:10:47 +0900

    issues#4対応

--- lib/batches/memcache_store.rb
+++ lib/batches/memcache_store.rb
@@ -8,32 +8,40 @@ require 'csv'
 class Batches::MemcacheStore
   def self.execute
     p " #{Time.now.to_s} ===== START ===== "
-    
-    # 地区
+
+    # 地区(大分類)
     store("area")
+    # 地区(小分類)
+    store("address")
     # 避難所
     store("shelter")
-    # 都道府県
-    store("state")
-    
-    i = 0
-    47.times do
-      i += 1
-      # 市区町村
-      store("city_#{sprintf("%02d", i)}")
-      # 町名
-      store("street_#{sprintf("%02d", i)}")
-    end
     
     p " #{Time.now.to_s} =====  END  ===== "
   end
   
   def self.store(file)
-    r = CSV.open("#{Rails.root}/lib/files/#{file}.csv", "r", encoding: "SJIS")
+    r = CSV.open("#{Rails.root}/lib/batches/#{file}.csv", "r", encoding: "utf8")
+    header = r.take(1)[0]
+
+    if file == "address"
+      state  = Hash.new
+      city   = Hash.new
+      street = Hash.new
+      r.each do |row|
+        state.store(row[0][0..1], row[1])
+        city.store(row[0][0..4], row[2])
+        street.store(row[0][0..7], row[3])
+      end
+      Rails.cache.write("state", state)
+      Rails.cache.write("city", city)
+      Rails.cache.write("street", street)
+    end
+
     h = Hash.new
     r.each do |row|
-      h.store(row[0], row[1])
+      h.store(row[0], Hash[*header.zip(row).flatten])
     end
     Rails.cache.write(file, h)
+
   end
 end
