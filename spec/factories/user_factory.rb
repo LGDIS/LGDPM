b3be75f94b683ba56b004a260411abdc05723301
c-yasuda <yasuda.chihiro@adniss.jp>
2013-02-28 16:50:41 +0900

    user validation対応

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,5 +2,8 @@
 FactoryGirl.define do
   factory :user, class: User do |user|
     user.login 'user'
+    user.email 'hoge@foo.co.jp'
+    user.password 'hogehogehoge'
+    user.confirmed_at Time.now
   end
 end
