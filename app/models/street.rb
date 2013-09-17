07be2eb73fd1fdcea9d2e15af7461d047ba1147f
suzuki-s <shinobusuzuki@di-system.co.jp>
2013-09-17 13:18:06 +0900

    bug fix

--- app/models/street.rb
+++ app/models/street.rb
@@ -4,17 +4,20 @@ class Street < ActiveRecord::Base
 
   # 町名ハッシュ取得処理
   # ==== Args
-  # _state_ :: 都道府県コード
+  # _state_ :: 市町村名
   # _term_ :: ユーザ入力値
   # ==== Return
   # 町名ハッシュオブジェクト {:code=>:name, :code=>:name}
   # ==== Raise
   def self.hash_for_table(state, term)
     streets_list = {}
-    streets = Street.where("code LIKE '#{state}%'").where("name LIKE '%#{term}%'").order(:code)
-    streets.each do |street|
-      streets_list[street.code] = {} unless streets_list[street.code]
-      streets_list[street.code] = street.name
+    city_id = City.where(:name => state).first
+    if city_id.present?
+      streets = Street.where("code LIKE '#{city_id.code}%'").where("name LIKE '%#{term}%'").order(:code)
+      streets.each do |street|
+        streets_list[street.code] = {} unless streets_list[street.code]
+        streets_list[street.code] = street.name
+      end
     end
     return streets_list
   end
